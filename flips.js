6c3a5834931d3e87828a8c42fe043e076e92da43
Andy King <andyking@live.unc.edu>
2022-02-14 16:06:54 -0500

    testing

--- flips.js
+++ flips.js
@@ -3,5 +3,7 @@ import { coinFlips } from "./modules/coin.mjs";
 import { countFlips } from './modules/coin.mjs';
 const args = minimist(process.argv);
 let out = coinFlips(args['number']);
-console.log(out)
-console.log(countFlips(out));
+let test = {heads: 1};
+console.log(test);
+// console.log(out)
+// console.log(countFlips(out));
